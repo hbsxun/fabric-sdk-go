52f19eb45f950db86cdec09254846edd088dc9e3
Your Name <you@example.com>
2017-08-04 10:02:46 +0800

    add assetApp

--- apiServer/controllers/assetApp/userManage.go
+++ apiServer/controllers/assetApp/userManage.go
@@ -3,6 +3,7 @@ package assetApp
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/astaxie/beego"
 	"github.com/hyperledger/fabric-sdk-go/apiServer/models/assetApp"
@@ -24,57 +25,201 @@ func (u *UserManageController) Register() {
 	var ur user.User
 	json.Unmarshal(u.Ctx.Input.RequestBody, &ur)
 	impl := assetApp.UserManagerImpl{}
-	uid, ok := impl.Register(&ur)
+	id, ok := impl.Register(&ur)
+	res := make(map[string]interface{})
 	if !ok {
-		u.Data["json"] = "register failed"
+		res["status"] = 301
+		res["message"] = "register failed"
 	} else {
-		u.Data["json"] = fmt.Sprintf("Register user successfully, uid:%d", uid)
+		res["status"] = 200
+		res["message"] = "register successfully"
+		res["userId"] = id
 	}
+	u.Data["json"] = res
 	u.ServeJSON()
 }
 
 // @Title Login
 // @Description Logs user into the system
-// @Param	username		query 	string	true		"The username for login"
-// @Param	password		query 	string	true		"The password for login"
+// @Param	body		body 	user.Secret	true		"body for user login"
 // @Success 200 {string} login successfully
-// @Failure 403 user not exist
-// @router /userLogin [get]
+// @Failure 403 user not exist or login failed
+// @router /userLogin [post]
 func (u *UserManageController) Login() {
-	username := u.GetString("username")
-	password := u.GetString("password")
-	fmt.Println("name:", username, "passwd:", password)
+	var ss user.Secret
+	json.Unmarshal(u.Ctx.Input.RequestBody, &ss)
+	fmt.Println("User Secret:", ss)
 	impl := assetApp.UserManagerImpl{}
-	signedToekn, err := impl.Login(username, password)
+	signedToekn, err := impl.Login(&ss)
+
+	fmt.Println("User Secret:", ss)
+	fmt.Println("Cookie Token:", signedToekn)
+	res := make(map[string]interface{})
 	if err != nil {
-		u.Data["json"] = "login failed!"
+		res["status"] = 302
+		res["message"] = "Login failed"
 	} else {
 		//for authorization
 		u.Ctx.SetCookie("Bearer", signedToekn)
-		tokenMap := make(map[string]interface{})
-		tokenMap["status"] = 200
-		tokenMap["message"] = "Login successfully"
-		u.Data["json"] = tokenMap
+		res["status"] = 200
+		res["message"] = "Login successfully"
 	}
+	u.Data["json"] = res
 	u.ServeJSON()
 }
 
 // @Title UpdateInfo
 // @Description update the user
-// @Param	body		body 	user.User	true		"body for user content"
+// @Param	body		body 	user.UpdateUserArgs	true		"body for user content"
 // @Success 200 {string} update successfully
 // @Failure 403 :uid is not int
 // @router /updateUser [put]
 func (u *UserManageController) UpdateInfo() {
-	var ur user.User
+	var ur user.UpdateUserArgs
 	json.Unmarshal(u.Ctx.Input.RequestBody, &ur)
 	fmt.Println("updateUser:", ur)
 	impl := assetApp.UserManagerImpl{}
 	err := impl.UpdateInfo(&ur)
+	res := make(map[string]interface{})
 	if err != nil {
-		u.Data["json"] = fmt.Sprintf("update failed:%s", err.Error())
+		res["status"] = 303
+		res["message"] = fmt.Sprintf("update failed:%s", err.Error())
 	} else {
-		u.Data["json"] = "update successfully"
+		res["status"] = 200
+		res["message"] = "User update successfully"
 	}
+	u.Data["json"] = res
+	u.ServeJSON()
+}
+
+// @Title UpdatePasswd
+// @Description update password
+// @Param	name		path 	string	true		"The name of user"
+// @Param	oldPassword		path 	string	true		"The old password of user"
+// @Param	newPassword		path 	string	true		"The new password of user"
+// @Success 200 {string}
+// @Failure 403 :name is empty
+// @router /UpdatePasswd/:name/:oldPassword/:newPassword [put]
+func (u *UserManageController) UpdatePasswd() {
+	name := u.GetString(":name")
+	fmt.Println("userName: ", name)
+	oldPwd := u.GetString(":oldPassword")
+	fmt.Println("oldPwd: ", oldPwd)
+	newPwd := u.GetString(":newPassword")
+	fmt.Println("newPwd: ", newPwd)
+	if name != "" && oldPwd != "" && newPwd != "" {
+		impl := assetApp.UserManagerImpl{}
+		err := impl.UpdatePwd(name, oldPwd, newPwd)
+		res := make(map[string]interface{})
+		if err != nil {
+			res["status"] = 304
+			res["message"] = fmt.Sprintf("update password:%s", err.Error())
+		} else {
+			res["status"] = 200
+			res["message"] = "update password successfully"
+		}
+		u.Data["json"] = res
+	}
+
+	u.ServeJSON()
+}
+
+// @Title GetUserByName
+// @Description get user by username
+// @Param	userName		path 	string	true		"The key for staticblock"
+// @Success 200 {object}user.User
+// @Failure 403 :userName is empty
+// @router /getUserByName/:userName [get]
+func (u *UserManageController) GetUserByName() {
+	name := u.GetString(":userName")
+	fmt.Println("userName: ", name)
+	if name != "" {
+		impl := assetApp.UserManagerImpl{}
+		userInfo, err := impl.GetUserInfoByName(name)
+		fmt.Println("userInfo: ", userInfo)
+		res := make(map[string]interface{})
+		if err != nil {
+			res["status"] = 304
+			res["message"] = fmt.Sprintf("get user by name failed:%s", err.Error())
+		} else {
+			res["status"] = 200
+			res["message"] = userInfo
+		}
+		u.Data["json"] = res
+	}
+
+	u.ServeJSON()
+}
+
+// @Title GetUserById
+// @Description get user by userid
+// @Param	userId		path 	string	true		"The key for staticblock"
+// @Success 200 {object}user.User
+// @Failure 403 :userId is empty
+// @router /getUserById/:userId [get]
+func (u *UserManageController) GetUserById() {
+	id := u.GetString(":userId")
+	fmt.Println("userId: ", id)
+	if id != "" {
+		res := make(map[string]interface{})
+		userId, err := strconv.Atoi(id)
+		if err != nil {
+			res["status"] = 304
+			res["message"] = fmt.Sprintf("atoi failed:%s", err.Error())
+		} else {
+			impl := assetApp.UserManagerImpl{}
+			userInfo, err := impl.GetUserInfoById(userId)
+			fmt.Println("userInfo: ", userInfo)
+			if err != nil {
+				res["status"] = 304
+				res["message"] = fmt.Sprintf("get user by id failed:%s", err.Error())
+			} else {
+				res["status"] = 200
+				res["message"] = userInfo
+			}
+		}
+		u.Data["json"] = res
+	}
+
+	u.ServeJSON()
+}
+
+// @Title Logout
+// @Description logout
+// @Success 200 {string}logout successfully
+// @Failure 403 :logout failed
+// @router /logout [get]
+func (u *UserManageController) Logout() {
+	u.Ctx.SetCookie("Bearer", "xxxx")
+	res := make(map[string]interface{})
+	res["status"] = 200
+	res["message"] = "logout successfully"
+
+	u.Data["json"] = res
+
+	u.ServeJSON()
+}
+
+// @Title VerifyUser
+// @Description verify user
+// @Param	body		body 	user.Secret	true		"body for user login"
+// @Success 200 {string} verify successfully
+// @Failure 403 user not exist or login failed
+// @router /VerifyUser [post]
+func (u *UserManageController) VerifyUser() {
+	var ss user.Secret
+	json.Unmarshal(u.Ctx.Input.RequestBody, &ss)
+	impl := assetApp.UserManagerImpl{}
+	err := impl.VerifyUser(&ss)
+	fmt.Println("User Secret:", ss)
+	res := make(map[string]interface{})
+	if err != nil {
+		res["status"] = 305
+		res["message"] = "Verify failed"
+	} else {
+		res["status"] = 200
+		res["message"] = "Verify successfully"
+	}
+	u.Data["json"] = res
 	u.ServeJSON()
 }
